177dfca39399d7f4538ba0b700f7dedf4f52a017
Virág Tamás <virag.tamas2@gmail.com>
2016-03-20 11:11:45 +0100

    Áttérés DOM-ra

--- HRPtoDB/src/DownloadTest.java
+++ HRPtoDB/src/DownloadTest.java
@@ -1,17 +1,63 @@
 import java.io.FileOutputStream;
-import javax.xml.soap.*;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import org.w3c.dom.Attr;
+import org.w3c.dom.Element;
+import org.w3c.dom.Document;
 
 public class DownloadTest {
 
 	public static void main(String[] args) {
 		try{
-			MessageFactory factory = MessageFactory.newInstance();
-			SOAPMessage message = factory.createMessage();
-			SOAPPart part = message.getSOAPPart();
+			DocumentBuilderFactory docfactory = DocumentBuilderFactory.newInstance();
+			DocumentBuilder docbuilder = docfactory.newDocumentBuilder();
 			
-			SOAPEnvelope envelope = part.getEnvelope();
-			SOAPHeader header = envelope.getHeader();
-			SOAPBody body = envelope.getBody();
+			/*
+			 * <Envelope>
+         	 *	 <Body>
+           	 *		<Request>
+             *			<Base>
+             * 				<AuthCode>##########</AuthCode>
+             *				<Language>hu</Language>
+             *				<Currency>HUF</Currency>
+             *			</Base>
+             *			<ProductId>...</ProductId>
+			 *		</Request>
+         	 *	 </Body>
+       		 * </Envelope>
+			 */
+			
+			//Envelope
+			Document doc = docbuilder.newDocument();
+			Element envelope = doc.createElement("Envelope");
+			doc.appendChild(envelope);
+			
+			//Body
+			
+			
+			//Request
+			
+			
+			//Base
+			
+			
+			//AuthCode
+			
+			
+			//Language
+			
+			
+			//Currency
+			
+			
+			//ProductID
+			
 			
 			
 		}
