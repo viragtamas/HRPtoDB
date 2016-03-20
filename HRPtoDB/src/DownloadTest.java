c12addc0926c4a78b47ca966607701f62aa4f8ca
Virág Tamás <virag.tamas2@gmail.com>
2016-03-20 10:20:07 +0100

    Soap message alap elemek

--- HRPtoDB/src/DownloadTest.java
+++ HRPtoDB/src/DownloadTest.java
@@ -1,8 +1,24 @@
+import java.io.FileOutputStream;
+import javax.xml.soap.*;
 
 public class DownloadTest {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		try{
+			MessageFactory factory = MessageFactory.newInstance();
+			SOAPMessage message = factory.createMessage();
+			SOAPPart part = message.getSOAPPart();
+			
+			SOAPEnvelope envelope = part.getEnvelope();
+			SOAPHeader header = envelope.getHeader();
+			SOAPBody body = envelope.getBody();
+			
+			
+		}
+		catch (Exception e){
+			e.printStackTrace();
+		}
+		
 
 	}
 
