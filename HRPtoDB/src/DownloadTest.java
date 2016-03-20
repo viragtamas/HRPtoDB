433656ec1009cdb9d9ee0b7c61c26d56a4d0dc0e
Virág Tamás <virag.tamas2@gmail.com>
2016-03-20 13:37:57 +0100

    XML összeállítás a GetCatalogueItem szolgáltatáshoz mintaként

--- HRPtoDB/src/DownloadTest.java
+++ HRPtoDB/src/DownloadTest.java
@@ -1,13 +1,10 @@
-import java.io.FileOutputStream;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.OutputKeys;
 import javax.xml.transform.Transformer;
-import javax.xml.transform.TransformerException;
 import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
-import org.w3c.dom.Attr;
 import org.w3c.dom.Element;
 import org.w3c.dom.Document;
 
@@ -18,7 +15,7 @@ public class DownloadTest {
 			DocumentBuilderFactory docfactory = DocumentBuilderFactory.newInstance();
 			DocumentBuilder docbuilder = docfactory.newDocumentBuilder();
 			
-			/*
+			/* Sample XML
 			 * <Envelope>
          	 *	 <Body>
            	 *		<Request>
@@ -39,26 +36,47 @@ public class DownloadTest {
 			doc.appendChild(envelope);
 			
 			//Body
-			
+			Element body = doc.createElement("Body");
+			envelope.appendChild(body);
 			
 			//Request
-			
+			Element request = doc.createElement("Request");
+			body.appendChild(request);
 			
 			//Base
-			
+			Element base = doc.createElement("Base");
+			request.appendChild(base);
 			
 			//AuthCode
-			
+			Element authcode = doc.createElement("AuthCode");
+			authcode.setTextContent("test");
+			base.appendChild(authcode);
 			
 			//Language
-			
+			Element language = doc.createElement("Language");
+			language.setTextContent("hu");
+			base.appendChild(language);
 			
 			//Currency
-			
+			Element currency = doc.createElement("Currency");
+			currency.setTextContent("HUF");
+			base.appendChild(currency);
 			
 			//ProductID
+			Element productid = doc.createElement("ProductId");
+			productid.setTextContent("123");
+			request.appendChild(productid);
 			
+			//Print result
+			TransformerFactory transformerFactory = TransformerFactory.newInstance();
+			Transformer transformer = transformerFactory.newTransformer();
+			DOMSource source = new DOMSource(doc);
 			
+			System.out.println("Request: ");
+			StreamResult result = new StreamResult(System.out);
+			transformer.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "yes");
+			transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+			transformer.transform(source, result);		
 			
 		}
 		catch (Exception e){
