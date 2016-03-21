bcaf86d52b0cbb07004d25fb67d9779ada9a5d45
Virág Tamás <virag.tamas2@gmail.com>
2016-03-21 19:23:55 +0100

    HrpXml osztályban metódusok létrehozása GetCatalogueItem metódus kidolgozása, DownloadTest-ből az XML összeállítási rész eltávolítása HrpXml példányostíás és metódus hívása

--- HRPtoDB/src/DownloadTest.java
+++ HRPtoDB/src/DownloadTest.java
@@ -1,101 +1,23 @@
-import java.io.StringWriter;
-
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.transform.OutputKeys;
-import javax.xml.transform.Transformer;
-import javax.xml.transform.TransformerFactory;
-import javax.xml.transform.dom.DOMSource;
-import javax.xml.transform.stream.StreamResult;
-import org.w3c.dom.Element;
-import org.w3c.dom.Document;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import org.apache.http.HttpResponse;
-import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
-import org.apache.http.client.methods.CloseableHttpResponse;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.entity.ContentType;
 import org.apache.http.entity.StringEntity;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.util.EntityUtils;
 
 public class DownloadTest {
 
 	public static void main(String[] args) {
 		try{
-			DocumentBuilderFactory docfactory = DocumentBuilderFactory.newInstance();
-			Document doc = docfactory.newDocumentBuilder().newDocument();
-			
-			/* Sample XML
-			 * <Envelope>
-         	 *	 <Body>
-           	 *		<Request>
-             *			<Base>
-             * 				<AuthCode>##########</AuthCode>
-             *				<Language>hu</Language>
-             *				<Currency>HUF</Currency>
-             *			</Base>
-             *			<ProductId>...</ProductId>
-			 *		</Request>
-         	 *	 </Body>
-       		 * </Envelope>
-			 */
-			
-			//Envelope
-			Element envelope = doc.createElement("Envelope");
-			doc.appendChild(envelope);
-			
-			//Body
-			Element body = doc.createElement("Body");
-			envelope.appendChild(body);
-			
-			//Request
-			Element request = doc.createElement("Request");
-			body.appendChild(request);
-			
-			//Base
-			Element base = doc.createElement("Base");
-			request.appendChild(base);
-			
-			//AuthCode
-			Element authcode = doc.createElement("AuthCode");
-			authcode.setTextContent("test");
-			base.appendChild(authcode);
-			
-			//Language
-			Element language = doc.createElement("Language");
-			language.setTextContent("hu");
-			base.appendChild(language);
-			
-			//Currency
-			Element currency = doc.createElement("Currency");
-			currency.setTextContent("HUF");
-			base.appendChild(currency);
-			
-			//ProductID
-			Element productid = doc.createElement("ProductId");
-			productid.setTextContent("123");
-			request.appendChild(productid);
-			
-			//Print result
-			Transformer transformer = TransformerFactory.newInstance().newTransformer();
-			DOMSource source = new DOMSource(doc);
-			
-			//Transforming request
-			System.out.println("Request: ");
-			StringWriter sw = new StringWriter();
-			//StreamResult result = new StreamResult(System.out);
-			transformer.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "yes");
-			transformer.setOutputProperty(OutputKeys.INDENT, "yes");
-			transformer.transform(source, new StreamResult(sw));
-			System.out.println(sw.toString());
-			
+			//Creating new Object to handle XML Creation
+			HrpXml XmlObject = new HrpXml("{123456}");
+						
 			//Creating HTTP Client
 			String strURL = "http://dev.hrp.hu/CompanyGroup.XmlGateway/CatalogueService/GetCatalogueItem/";
 			CloseableHttpClient httpclient = HttpClients.createDefault();
 			HttpPost post = new HttpPost(strURL);
-			post.setEntity(new StringEntity(sw.toString()));
+			post.setEntity(new StringEntity(XmlObject.GetCatalogueItem()));
 			
 			//Post XML to URL
 			HttpResponse response = httpclient.execute(post);
@@ -105,7 +27,19 @@ public class DownloadTest {
 		    //Get Response
 		    System.out.println("Response Code : " + response.getStatusLine().getStatusCode());
 
-		
+		    BufferedReader rd = new BufferedReader(
+                    new InputStreamReader(response.getEntity().getContent()));
+
+		    StringBuffer result = new StringBuffer();
+		    String line = "";
+		    while ((line = rd.readLine()) != null) {
+		        result.append(line);
+		        System.out.println(line.toString());
+		    }
+
+		    
+		    
+		    
 		}
 		catch (Exception e){
 			e.printStackTrace();
--- HRPtoDB/src/HrpXml.java
+++ HRPtoDB/src/HrpXml.java
@@ -1,3 +1,4 @@
+import java.io.StringWriter;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.transform.OutputKeys;
 import javax.xml.transform.Transformer;
@@ -8,24 +9,119 @@ import org.w3c.dom.Element;
 import org.w3c.dom.Document;
 
 public class HrpXml {
-
-	public HrpXml(){
-		
+	
+	//AuthCode az azonosításhoz
+	public String AuthCode;
+	public String Query;
+	
+	public HrpXml(String AuthCode){
+		this.AuthCode = AuthCode;
 	}
 	
-	public StreamResult getCatalogueItem(){
+	public String GetCatalogueList(String url){
+		return Query;
+	}
+		
+	public String GetCatalogueItem(){
+		StringWriter sw = new StringWriter();
 		try{
 			DocumentBuilderFactory docfactory = DocumentBuilderFactory.newInstance();
 			Document doc = docfactory.newDocumentBuilder().newDocument();
 			
+			/* Sample XML
+			 * <Envelope>
+         	 *	 <Body>
+           	 *		<Request>
+             *			<Base>
+             * 				<AuthCode>##########</AuthCode>
+             *				<Language>hu</Language>
+             *				<Currency>HUF</Currency>
+             *			</Base>
+             *			<ProductId>...</ProductId>
+			 *		</Request>
+         	 *	 </Body>
+       		 * </Envelope>
+			 */
 			
-		}catch (Exception e){
-			e.printStackTrace(System.out);
+			//Envelope
+			Element envelope = doc.createElement("Envelope");
+			doc.appendChild(envelope);
+			
+			//Body
+			Element body = doc.createElement("Body");
+			envelope.appendChild(body);
+			
+			//Request
+			Element request = doc.createElement("Request");
+			body.appendChild(request);
+			
+			//Base
+			Element base = doc.createElement("Base");
+			request.appendChild(base);
+			
+			//AuthCode
+			Element authcode = doc.createElement("AuthCode");
+			authcode.setTextContent("test");
+			base.appendChild(authcode);
+			
+			//Language
+			Element language = doc.createElement("Language");
+			language.setTextContent("hu");
+			base.appendChild(language);
+			
+			//Currency
+			Element currency = doc.createElement("Currency");
+			currency.setTextContent("HUF");
+			base.appendChild(currency);
+			
+			//ProductID
+			Element productid = doc.createElement("ProductId");
+			productid.setTextContent("123");
+			request.appendChild(productid);
+			
+			//Print result
+			Transformer transformer = TransformerFactory.newInstance().newTransformer();
+			DOMSource source = new DOMSource(doc);
+			
+			//Transforming request
+			System.out.println("Request: ");
+			
+			transformer.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "yes");
+			transformer.setOutputProperty(OutputKeys.INDENT, "no");
+			transformer.transform(source, new StreamResult(sw));
+			System.out.println(sw.toString());
+		}catch(Exception e){
+			e.printStackTrace();
 		}
-		StreamResult CatalogueItem = new StreamResult();
-		return CatalogueItem;
-		
+		return sw.toString();
 	}
 	
+	public String GetStructurePta(){
+		return Query;
+	}
+	
+	public String GetInvoiceList(){
+		return Query;
+	}
+	
+	public String GetInvoiceDetails(){
+		return Query;
+	}
+	
+	public String GetStructureList(){
+		return Query;
+	}
+	
+	public String GetDeliveryAddresses(){
+		return Query;
+	}
+	
+	public String GetOrderInfo(){
+		return Query;
+	}
+	
+	public String CreateOrder(){
+		return Query;
+	}
 	
 }
